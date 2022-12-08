574de4d081e87542d15cfbacfa3be55fe8dfe4d9
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-08 16:00:33 +0530

    studeunt app spring

---
+++ src/main/java/com/example/SpringStudentApp_new/SpringStudentAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringStudentApp_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentAppNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentAppNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringStudentApp_new/SpringStudentAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringStudentApp_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentAppNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
